2f450a07d9e7cd6124210c436a4395f7a979450a
FikratRH <fikratrh@code.edu.az>
2023-07-20 14:15:55 +0400

    completed tasks

---
+++ TasksFrom3To8/Program.cs
@@ -0,0 +1,72 @@
+﻿#region Task 3 Find the sum of numbers between given two
+//Console.WriteLine("Please,enter the first number:");
+//int firstNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please,enter the second number:");
+//int secondNum = int.Parse(Console.ReadLine());
+//int sum = 0;
+
+//for (int i = firstNum;i <= secondNum; i++)
+//{
+//    sum += i;
+//}
+//Console.WriteLine($"The sum of numbers between {firstNum} and {secondNum} is {sum}");
+#endregion
+
+#region Task 4 Custom Math.Pow
+//Console.WriteLine("Please,enter the first number:");
+//int firstNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please,enter the second number:");
+//int secondNum = int.Parse(Console.ReadLine());
+//int result = 1;
+
+//for (int i = 0;i < secondNum; i++)
+//{
+//    result *= firstNum;
+//}
+//Console.WriteLine($"{firstNum} powered by {secondNum} is {result}");
+
+
+#endregion
+
+#region Task 5 Finding the number of digits of a given number
+//Console.WriteLine("Please,the number:");
+//int num = int.Parse(Console.ReadLine());
+//int digitCount = 0;
+
+//while (num > 0)
+//{
+//    num /= 10;
+//    digitCount++;
+//}
+//Console.WriteLine($"The given number is a {digitCount}-digit number");
+#endregion
+
+#region Task 6 Filtering the string array with elements' lengths more than 4
+//Console.WriteLine("Enter strings you want to add to an array (with a space between them)");
+//string[] words = Console.ReadLine().Split();
+//string[] filteredArr = words.Where(word=> word.Length > 4).ToArray();
+
+//Console.WriteLine($"List of words with length more than 4:\n{string.Join(" ", filteredArr)}");
+#endregion
+
+#region Task 7 Fibonacci numbers
+
+//Console.WriteLine("Please,enter the first fibonacci number:");
+//int firstNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please,enter the second fibonacci number:");
+//int secondNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please,enter the amount of numbers you want to type");
+
+//int thirdNum = 0;
+
+//int count = int.Parse(Console.ReadLine());
+
+//Console.Write(firstNum + " " + secondNum + " ");
+//for (int i = 0;i < count; i++)
+//{
+//    thirdNum = firstNum + secondNum;
+//    Console.Write(thirdNum + " ");
+//    firstNum = secondNum;
+//    secondNum = thirdNum;
+//}
+#endregion
